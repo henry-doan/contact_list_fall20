c89ca3bcb0a3399bd9119582efeeeb79f0b99706
Henry Doan <henrydoan96@gmail.com>
2020-09-01 19:40:56 -0600

    added notes and addresses, and assoc

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,16 @@
 class Contact < ApplicationRecord
+  has_many :notes, dependent: :destroy
+  has_one :address, dependent: :destroy
+
+  # Associations 
+    # belongs_to - childs
+    # has_many - parent
+    # has_one - parent
+    # has_many :through
+    # has_one :through
+    # dependent, has_one, has_many
+
+  # Validations
+  
+  # Methods
 end
